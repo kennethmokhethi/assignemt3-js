e5868843a79e80dc516fb6251967d64a12eb52d4
kenneth Mahlatse <kennethmahlatsemokhethi@gmail.com>
2022-06-04 18:48:29 +0200

    fixes

--- MainProject/script.js
+++ MainProject/script.js
@@ -13,21 +13,21 @@ const computerPlay = () => {
 const playRound = (playerSelection, computerSelection) => {
     if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
         return "Tie";
-    } else if (playerSelection.toLowerCase() == "rock") {
-        if (computerSelection.toLowerCase() == "paper") {
+    } else if (playerSelection == "rock") {
+        if (computerSelection == "paper") {
             return "You Lose! Paper beats Rock";
         } else {
             return "You Win! Rock beats Scissor";
         }
-    } else if (playerSelection.toLowerCase() == "paper") {
-        if (computerSelection.toLowerCase() == "rock") {
+    } else if (playerSelection == "paper") {
+        if (computerSelection == "rock") {
             return "You Win! Paper beats Rock";
         } else {
             return "You Lose! Scissor beats Paper"
         }
 
-    } else if (playerSelection.toLowerCase() == "scissor") {
-        if (computerSelection.toLowerCase() == "rock") {
+    } else if (playerSelection == "scissor") {
+        if (computerSelection == "rock") {
             return "You Lose! Rock beats Scissor"
         } else {
             return "You Win! Scissor beats Paper"
@@ -43,8 +43,8 @@ const game = () => {
     let computerScore = 0;
     for (let i = 0; i < 5; i++) {
         console.log("--------------------Round", i + 1, "-----------------------");
-        const playerSelection = prompt("Enter your selction:Rock or Paper or Scissors")
-        const computerSelection = computerPlay();
+        const playerSelection = prompt("Enter your selction:Rock or Paper or Scissors").toLowerCase();
+        const computerSelection = computerPlay().toLowerCase();
         console.log("Computer selection", computerSelection);
         console.log("player  selection", playerSelection);
         const gameOutcome = playRound(playerSelection, computerSelection);
--- MathematicalShaps.js
+++ MathematicalShaps.js
@@ -1,15 +1,15 @@
 //2 a.
-const squareArea = (length) => length * length;
-console.log("Area of square where the length is 9 =", squareArea(9));
+const squareDiagonalLength = (length) => length * Math.sqrt(2);
+console.log("Area of square where the length is 9 =", squareDiagonalLength(9));
 
 //2 b.
-const triangleArea = (lengthA, lengthB, lengthC) => lengthA * lengthB * lengthC;
+const triangleArea = (lengthA, lengthB, lengthC) => (lengthA * lengthB * lengthC) / 2;
 console.log("Area of triangle where length of its sides is 5,6,7 =", triangleArea(5, 6, 7));
 
 //2 c i
 const PI = 3.14;
 const circumference = (radius) => 2 * PI * radius;
-console.log("Circuference ofd circele whos eradius is 4 =", circumference(4));
+console.log("Circumference of circle whose eradius is 4 =", circumference(4));
 
 const surfaceArea = (radius) => PI * (radius * radius);
 console.log("Surface area of circle whose radius is 4 =", surfaceArea(4));
